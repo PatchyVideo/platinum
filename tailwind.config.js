f41c21f406ebcd521567e1fc1c830e90234c8253
wrzrmzx <1510033076@qq.com>
2021-02-20 22:11:23 +0800

    chore: :lipstick: update tailwind color config for tag categories

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
       screens: {
         '3xl': '1920px',
       },
+      colors: {
+        copyright: '#a0a',
+        language: '#585455',
+        character: '#0a0',
+        author: '#a00',
+        general: '0073ff',
+        meta: '#f80',
+        soundtrack: '#ff7792',
+      },
     },
   },
   plugins: [
