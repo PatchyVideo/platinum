ebb5b6b092a0bbcada27b659579501c6f3f442f2
wrzrmzx <1510033076@qq.com>
2021-02-21 10:41:58 +0800

    chore(nav): :sparkles: top nav link to search page on mobile

--- packages/common/components/NavTop.vue
+++ packages/common/components/NavTop.vue
@@ -19,20 +19,22 @@
         <logo class="hidden md:inline-block"></logo>
       </div>
       <!-- Search Bar -->
-      <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="black"
-          width="30px"
-          height="30px"
-          class="fill-current inline md:hidden"
-        >
-          <path d="M0 0h24v24H0V0z" fill="none" />
-          <path
-            d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
-          />
-        </svg>
+      <div v-if="showSearchBar">
+        <router-link to="/search-page">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="black"
+            width="30px"
+            height="30px"
+            class="fill-current inline md:hidden"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path
+              d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+            />
+          </svg>
+        </router-link>
         <AutoComplete class="hidden md:inline-block"></AutoComplete>
       </div>
       <!-- User -->
@@ -121,7 +123,12 @@ export default defineComponent({
     Logo,
     AutoComplete,
   },
-  props: {},
+  props: {
+    showSearchBar: {
+      type: Boolean,
+      default: true,
+    },
+  },
   setup() {
     const { t } = useI18n()
     let isLogin = ref(false)
