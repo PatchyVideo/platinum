78747c0c6bb2a4219dc8febc7503655be94df617
wrzrmzx <1510033076@qq.com>
2021-02-21 11:56:05 +0800

    chore(search): :globe_with_meridians: search page i18n

--- packages/search/SearchPage.vue
+++ packages/search/SearchPage.vue
@@ -2,10 +2,12 @@
   <div class="bg-pink-50 dark:bg-gray-700 dark:text-gray-400">
     <nav-top :show-search-bar="false"></nav-top>
     <div class="pt-26 pb-30 text-center">
-      <h3 class="text-lg font-semibold p-4 px-auto dark:text-white">PatchyVideo</h3>
+      <h3 class="text-lg font-semibold p-4 px-auto dark:text-white">
+        {{ 'PatchyVideo | ' + t('common.autoComplete.search') }}
+      </h3>
       <auto-complete size="mobile" class="inline-block"></auto-complete>
     </div>
-    <!-- <Footer></Footer> -->
+    <Footer></Footer>
   </div>
 </template>
 
@@ -13,14 +15,15 @@
 import AutoComplete from '@/common/components/AutoComplete.vue'
 import NavTop from '@/common/components/NavTop.vue'
 import Footer from '@/common/components/Footer.vue'
-import { onMounted, onUnmounted, reactive, ref, defineComponent } from 'vue'
-// import { useI18n } from 'vue-i18n'
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   components: { NavTop, AutoComplete, Footer },
   props: {},
   setup() {
-    return {}
+    const { t } = useI18n()
+    return { t }
   },
 })
 </script>
