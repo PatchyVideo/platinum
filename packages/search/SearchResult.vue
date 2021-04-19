bf8943d1e1c66f84f8c3a1f258e9d68d83ca3655
wrzrmzx <1510033076@qq.com>
2021-04-19 22:17:29 +0800

    feat(search): :construction: add pagination

--- packages/search/SearchResult.vue
+++ packages/search/SearchResult.vue
@@ -16,6 +16,7 @@
         <div>加载失败了QAQ</div>
         <div>{{ '错误原因：' + errMsg }}</div>
       </div>
+      <div v-else-if="count === 0">没有搜索到视频QAQ</div>
       <div v-else-if="status === 'result'">
         <div>{{ '共搜索到' + count + '个视频' }}</div>
         <div
@@ -34,15 +35,36 @@
             <div v-if="video.item.partName">
               <a class="inline-block w-full truncate" :title="video.item.title">{{ video.item.title }}</a>
               <div class="text-xs inline-block w-full truncate text-gray-600">
-                <label class="font-semibold">P{{ pageOfVideo(video.item.url) }}</label
+                <label class="font-semibold">{{ 'P' + pageOfVideo(video.item.url) + ':' }}</label
                 >{{ video.item.partName }}
               </div>
             </div>
-            <a v-else class="title overflow-ellipsis overflow-hidden" :title="video.item.title">{{
+            <a v-else href="#" class="title overflow-ellipsis overflow-hidden" :title="video.item.title">{{
               video.item.title
             }}</a>
             <div>{{ '源网站：' + video.item.site }}</div>
           </div>
+        </div>
+        <div class="flex-1 flex justify-between items-center">
+          <a
+            v-if="offset"
+            href="#"
+            class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
+            @click="jumpToPreviousPage"
+          >
+            上一页
+          </a>
+          <a v-else></a>
+          <div>{{ '第' + (offset + 1) + '/' + pageCount + '页' }}</div>
+          <a
+            v-if="offset != pageCount"
+            href="#"
+            class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
+            @click="jumpToNextPage"
+          >
+            下一页
+          </a>
+          <a v-else></a>
         </div>
       </div>
     </div>
@@ -72,16 +94,16 @@ export default defineComponent({
   props: {},
   setup() {
     const { t } = useI18n()
-
-    /* Get the query Word and query result */
+    const route = useRoute()
+    const router = useRouter()
     const limit = 20
-    const offset = ref(0)
     const status = ref<Status>()
     const errMsg = ref('')
     const count = ref(0)
     const pageCount = ref(0)
     const videos = ref<Video[]>()
-    const route = useRoute()
+
+    /* Precess URL query */
     const queryWord = computed(() => {
       const query = route.query.i ? (typeof route.query.i === 'object' ? route.query.i.join(' ') : route.query.i) : ''
       if (query) {
@@ -89,56 +111,77 @@ export default defineComponent({
       }
       return query
     })
+    const offsetChangeFromOtherQuery = ref(false)
+    const offset = computed(() =>
+      Number(
+        route.query.page ? (typeof route.query.page === 'object' ? route.query.page.join(' ') : route.query.page) : 0
+      )
+    )
+
+    /* Refresh query result for URL query change */
     watch(
       queryWord,
-      async () => {
-        if (!queryWord.value || status.value === Status.loading) return
-        status.value = Status.loading
-        try {
-          const res = await useQuery({
-            query: gql`
-              query($offset: Int, $limit: Int, $query: String) {
-                listVideo(para: { offset: $offset, limit: $limit, humanReadableTag: true, query: $query }) {
-                  count
-                  pageCount
-                  videos {
-                    id
-                    item {
-                      coverImage
-                      title
-                      site
-                      cid
-                      partName
-                      url
-                    }
-                  }
-                }
-              }
-            `,
-            variables: {
-              offset: offset.value,
-              limit: limit,
-              query: queryWord.value,
-            },
-          })
-          console.log(res)
-
-          const resultData = res.data.listVideo
-          count.value = resultData.count
-          pageCount.value = resultData.pageCount
-          videos.value = resultData.videos
-
-          status.value = Status.result
-        } catch (err) {
-          status.value = Status.error
-          // console.log(err)
-          errMsg.value = err.message
-        }
+      () => {
+        console.log('queryWord changed!')
+        queryVideos()
       },
       {
         immediate: true,
       }
     )
+    watch(offset, () => {
+      if (offsetChangeFromOtherQuery.value) {
+        offsetChangeFromOtherQuery.value = false
+        return
+      }
+      console.log('offset changed!')
+      queryVideos()
+    })
+
+    async function queryVideos(): Promise<void> {
+      if (!queryWord.value || status.value === Status.loading) return
+      status.value = Status.loading
+      try {
+        const res = await useQuery({
+          query: gql`
+            query($offset: Int, $limit: Int, $query: String) {
+              listVideo(para: { offset: $offset, limit: $limit, humanReadableTag: true, query: $query }) {
+                count
+                pageCount
+                videos {
+                  id
+                  item {
+                    coverImage
+                    title
+                    site
+                    cid
+                    partName
+                    url
+                  }
+                }
+              }
+            }
+          `,
+          variables: {
+            offset: offset.value * limit,
+            limit: limit,
+            query: queryWord.value,
+          },
+        })
+        // console.log(res)
+
+        const resultData = res.data.listVideo
+        count.value = resultData.count
+        pageCount.value = resultData.pageCount
+        videos.value = resultData.videos
+
+        status.value = Status.result
+      } catch (err) {
+        // console.log(err)
+        errMsg.value = err.message
+        status.value = Status.error
+      }
+    }
 
     /* Get part name number from Bilibili video's URL */
     const pageOfVideo = computed(() => {
@@ -148,9 +191,15 @@ export default defineComponent({
     })
 
     /* Change the router query to trigger the search function */
-    const router = useRouter()
     function searchResult(searchContent: string): void {
-      router.push({ path: '/search-result', query: { i: searchContent } })
+      offsetChangeFromOtherQuery.value = true
+      router.push({ path: '/search-result', query: { i: searchContent, page: 0 } })
+    }
+    function jumpToPreviousPage(): void {
+      router.push({ path: '/search-result', query: { i: queryWord.value, page: offset.value - 1 } })
+    }
+    function jumpToNextPage(): void {
+      router.push({ path: '/search-result', query: { i: queryWord.value, page: offset.value + 1 } })
     }
 
     /* Jump to video detail page */
@@ -159,15 +208,17 @@ export default defineComponent({
     }
     return {
       t,
+      queryWord,
       offset,
       status,
       errMsg,
       count,
       pageCount,
       videos,
-      queryWord,
       pageOfVideo,
       searchResult,
+      jumpToPreviousPage,
+      jumpToNextPage,
       jumpToVideoResult,
     }
   },
