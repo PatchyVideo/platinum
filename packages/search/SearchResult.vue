58625aa17400311cfaca36677dfdfc68ede04aec
wrzrmzx <1510033076@qq.com>
2021-06-14 20:16:52 +0800

    refactor(search): :recycle: use setup on search result

--- packages/search/SearchResult.vue
+++ packages/search/SearchResult.vue
@@ -71,7 +71,7 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import AutoComplete from '@/search/components/AutoComplete.vue'
 import NavTop from '@/common/components/NavTop.vue'
 import Footer from '@/common/components/Footer.vue'
@@ -79,119 +79,90 @@ import BackTop from '@/ui/components/BackTop.vue'
 import SearchVideo from '@/search/components/SearchVideo.vue'
 import SearchPlaylist from '@/search/components/SearchPlaylist.vue'
 import PvPagination from '@/ui/components/PvPagination.vue'
-import { computed, defineComponent, ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { setSiteTitle } from '@/common/lib/setSiteTitle'
 import { screenSizes } from '@/tailwindcss'
 
-export default defineComponent({
-  components: { NavTop, AutoComplete, Footer, BackTop, SearchVideo, PvPagination, SearchPlaylist },
-  props: {},
-  setup() {
-    const { t } = useI18n()
-    const route = useRoute()
-    const router = useRouter()
-    const pageCount = ref(0)
+const { t } = useI18n()
+const route = useRoute()
+const router = useRouter()
+const pageCount = ref(0)
 
-    /* Precess URL query */
-    const queryWord = computed(() => {
-      const query = String(
-        route.query.i ? (Array.isArray(route.query.i) ? route.query.i.join(' ') : route.query.i) : ''
-      )
-      if (query) {
-        setSiteTitle(t('search.search-result.title') + query)
-      }
-      return query
-    })
-    const offset = computed(() => {
-      return Number(route.query.page ? (Array.isArray(route.query.page) ? route.query.page[0] : route.query.page) : 0)
-    })
-
-    const page = computed(() => offset.value + 1)
-    const Tabs = [
-      {
-        value: 'video',
-        name: t('search.search-result.video.tab'),
-      },
-      {
-        value: 'playlist',
-        name: t('search.search-result.playlist.tab'),
-      },
-    ]
-    const tab = computed(() =>
-      String(route.query.tab ? (Array.isArray(route.query.tab) ? route.query.tab[0] : route.query.tab) : Tabs[0].value)
-    )
-
-    const Orders = [
-      { value: 'last_modified', name: t('search.search-result.order.last-modified') },
-      { value: 'video_oldest', name: t('search.search-result.order.video-oldest') },
-    ]
-    const order = computed(() =>
-      String(
-        route.query.order
-          ? Array.isArray(route.query.order)
-            ? route.query.order[0]
-            : route.query.order
-          : Orders[0].value
-      )
-    )
-
-    /* Change the router query to trigger the search function */
-    function searchResult(searchContent: string): void {
-      const query = JSON.parse(JSON.stringify(route.query))
-      query.i = searchContent
-      query.page = 0
-      router.push({ path: route.path, query })
-    }
-    function jumpToPreviousPage(): void {
-      const query = JSON.parse(JSON.stringify(route.query))
-      query.page = offset.value - 1
-      router.push({ path: route.path, query })
-    }
-    function jumpToNextPage(): void {
-      const query = JSON.parse(JSON.stringify(route.query))
-      query.page = offset.value + 1
-      router.push({ path: route.path, query })
-    }
-    function jumpToSelectedPage(page: number): void {
-      const query = JSON.parse(JSON.stringify(route.query))
-      query.page = page - 1
-      router.push({ path: route.path, query })
-    }
-    function changeTab(value: string): void {
-      const query = JSON.parse(JSON.stringify(route.query))
-      query.page = 0
-      query.tab = value
-      delete query.visible_site
-      delete query.order
-      router.push({ path: route.path, query })
-    }
-    function changeOrder(value: string): void {
-      const query = JSON.parse(JSON.stringify(route.query))
-      query.page = 0
-      query.order = value
-      router.push({ path: route.path, query })
-    }
-    return {
-      t,
-      screenSizes,
-      queryWord,
-      Tabs,
-      tab,
-      Orders,
-      order,
-      page,
-      pageCount,
-      searchResult,
-      jumpToPreviousPage,
-      jumpToNextPage,
-      jumpToSelectedPage,
-      changeTab,
-      changeOrder,
-    }
-  },
+/* Precess URL query */
+const queryWord = computed(() => {
+  const query = String(route.query.i ? (Array.isArray(route.query.i) ? route.query.i.join(' ') : route.query.i) : '')
+  if (query) {
+    setSiteTitle(t('search.search-result.title') + query)
+  }
+  return query
 })
+const offset = computed(() => {
+  return Number(route.query.page ? (Array.isArray(route.query.page) ? route.query.page[0] : route.query.page) : 0)
+})
+
+const page = computed(() => offset.value + 1)
+const Tabs = [
+  {
+    value: 'video',
+    name: t('search.search-result.video.tab'),
+  },
+  {
+    value: 'playlist',
+    name: t('search.search-result.playlist.tab'),
+  },
+]
+const tab = computed(() =>
+  String(route.query.tab ? (Array.isArray(route.query.tab) ? route.query.tab[0] : route.query.tab) : Tabs[0].value)
+)
+
+const Orders = [
+  { value: 'last_modified', name: t('search.search-result.order.last-modified') },
+  { value: 'video_oldest', name: t('search.search-result.order.video-oldest') },
+]
+const order = computed(() =>
+  String(
+    route.query.order ? (Array.isArray(route.query.order) ? route.query.order[0] : route.query.order) : Orders[0].value
+  )
+)
+
+/* Change the router query to trigger the search function */
+function searchResult(searchContent: string): void {
+  const query = JSON.parse(JSON.stringify(route.query))
+  query.i = searchContent
+  query.page = 0
+  router.push({ path: route.path, query })
+}
+function jumpToPreviousPage(): void {
+  const query = JSON.parse(JSON.stringify(route.query))
+  query.page = offset.value - 1
+  router.push({ path: route.path, query })
+}
+function jumpToNextPage(): void {
+  const query = JSON.parse(JSON.stringify(route.query))
+  query.page = offset.value + 1
+  router.push({ path: route.path, query })
+}
+function jumpToSelectedPage(page: number): void {
+  const query = JSON.parse(JSON.stringify(route.query))
+  query.page = page - 1
+  router.push({ path: route.path, query })
+}
+function changeTab(value: string): void {
+  const query = JSON.parse(JSON.stringify(route.query))
+  query.page = 0
+  query.tab = value
+  delete query.visible_site
+  delete query.order
+  router.push({ path: route.path, query })
+}
+function changeOrder(value: string): void {
+  const query = JSON.parse(JSON.stringify(route.query))
+  query.page = 0
+  query.order = value
+  router.push({ path: route.path, query })
+}
 </script>
 
 <style lang="postcss" scoped>
