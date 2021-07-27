65275b8bb77d28965451dce6542b93965ab7bab9
wrzrmzx <1510033076@qq.com>
2021-07-27 22:20:54 +0800

    feat(user): :sparkles: notification page readonly part complete

--- packages/user/Notification.vue
+++ packages/user/Notification.vue
@@ -102,7 +102,6 @@
           transition-transform
           duration-300
           w-9/12
-          md:w-auto
           dark:bg-gray-700
         "
         :class="{ 'translate-x-full': !noteDrawerOpen }"
@@ -114,7 +113,7 @@
         <div class="mt-4 space-y-2">
           <div
             class="flex align-middle p-2 rounded-md"
-            :class="{ 'bg-gray-50 shadow-inner': noteType === 'comment_reply' }"
+            :class="{ 'bg-gray-50 shadow-inner dark:bg-gray-500': noteType === 'comment_reply' }"
             @click="changeNoteType('comment_reply')"
           >
             <icon-uil-comment-alt-dots class="inline align-middle w-7 text-lg text-center" />
@@ -134,14 +133,14 @@
           </div>
           <div
             class="flex align-middle p-2 rounded-md text-gray-500"
-            :class="{ 'bg-gray-50 shadow-inner': noteType === 'at' }"
+            :class="{ 'bg-gray-50 shadow-inner dark:bg-gray-500': noteType === 'at' }"
           >
             <icon-uil-at class="inline align-middle w-7 text-lg text-center" />
             <div>艾特我的（建设中）</div>
           </div>
           <div
             class="flex align-middle p-2 rounded-md"
-            :class="{ 'bg-gray-50 shadow-inner': noteType === 'system_message' }"
+            :class="{ 'bg-gray-50 shadow-inner dark:bg-gray-500': noteType === 'system_message' }"
             @click="changeNoteType('system_message')"
           >
             <icon-uil-volume class="inline align-middle w-7 text-lg text-center" />
@@ -167,9 +166,142 @@
           v-if="noteDrawerOpen"
           class="fixed inset-0 bg-black bg-opacity-20 z-49"
           @click="noteDrawerOpen = false"
-          @touchmove.prevent.passive
         ></div>
       </Transition>
+    </div>
+    <!-- Desktop View -->
+    <div v-else class="p-5 flex space-x-3 min-h-screen w-9/10 m-auto xl:w-4/5">
+      <div class="p-1 overflow-auto rounded-md shadow bg-white w-1/4 xl:w-1/5 dark:bg-gray-700">
+        <div class="w-full border-b p-1 pb-1.5 flex items-center flex-nowrap">
+          <icon-uil-telegram-alt class="text-2xl transition-colors hover:bg-gray-200 hover:dark:bg-gray-900" />
+          <div class="text-lg ml-2">消息中心</div>
+        </div>
+        <div class="mt-4 space-y-2">
+          <div
+            class="flex align-middle p-2 rounded-md cursor-pointer"
+            :class="{ 'bg-gray-50 shadow-inner dark:bg-gray-500': noteType === 'comment_reply' }"
+            @click="changeNoteType('comment_reply')"
+          >
+            <icon-uil-comment-alt-dots class="inline align-middle w-7 text-lg text-center" />
+            <div>
+              回复我的<label
+                v-if="listNoteCountTypes?.find((type) => type.msgtype === 'comment_reply')?.count"
+                class="bg-red-500 text-white text-sm rounded-full px-2"
+                >{{
+                  listNoteCountTypes?.find((type) => type.msgtype === 'comment_reply')?.count
+                    ? listNoteCountTypes?.find((type) => type.msgtype === 'comment_reply')?.count
+                    : 0 > 99
+                    ? '99+'
+                    : listNoteCountTypes.find((type) => type.msgtype === 'comment_reply')?.count
+                }}</label
+              >
+            </div>
+          </div>
+          <div
+            class="flex align-middle p-2 rounded-md text-gray-500 cursor-pointer"
+            :class="{ 'bg-gray-50 shadow-inner dark:bg-gray-500': noteType === 'at' }"
+          >
+            <icon-uil-at class="inline align-middle w-7 text-lg text-center" />
+            <div>艾特我的（建设中）</div>
+          </div>
+          <div
+            class="flex align-middle p-2 rounded-md cursor-pointer"
+            :class="{ 'bg-gray-50 shadow-inner dark:bg-gray-500': noteType === 'system_message' }"
+            @click="changeNoteType('system_message')"
+          >
+            <icon-uil-volume class="inline align-middle w-7 text-lg text-center" />
+            <div>
+              系统通知<label
+                v-if="listNoteCountTypes?.find((type) => type.msgtype === 'system_message')?.count"
+                class="bg-red-500 text-white text-sm rounded-full px-2"
+                >{{
+                  listNoteCountTypes?.find((type) => type.msgtype === 'system_message')?.count
+                    ? listNoteCountTypes?.find((type) => type.msgtype === 'system_message')?.count
+                    : 0 > 99
+                    ? '99+'
+                    : listNoteCountTypes.find((type) => type.msgtype === 'system_message')?.count
+                }}</label
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-3/4 p-2 bg-white rounded-md shadow xl:4/5 dark:bg-gray-600">
+        <div v-if="loading">加载中</div>
+        <div v-else-if="listNoteCountAll == 0"></div>
+        <div v-else-if="noteType === 'comment_reply'">
+          <div class="border-b flex justify-between p-1">
+            <div>回复我的</div>
+            <div class="text-sm cursor-pointer" :class="{ 'text-gray-500': !listNoteCountUnread }">全部标为已读</div>
+          </div>
+          <div v-for="note in listNote" :key="note.id.id">
+            <div
+              v-if="note.__typename === 'ReplyNotificationObject'"
+              class="flex items-center m-1 p-2 shadow rounded-md"
+              :class="{ 'bg-gray-100 dark:bg-gray-500': !note.read }"
+            >
+              <router-link class="w-1/10 cursor-pointer xl:w-1/15" to>
+                <UserAvatar
+                  :title="note.repliedBy.username"
+                  :image="note.repliedBy.image"
+                  class="rounded-full ring-2 ring-white"
+                ></UserAvatar>
+              </router-link>
+              <router-link
+                :to="
+                  (note.repliedType === 'forum' ? '' : note.repliedType === 'video' ? '/video/' : '/playlist/') +
+                  note.repliedObj +
+                  '#' +
+                  note.cid
+                "
+                tag="div"
+                class="w-9/10 xl:w-14/15"
+              >
+                <div>
+                  {{ note.repliedBy.username + ' 回复了你：' }}
+                </div>
+                <div class="text-xs bg-gray-100 text-gray-400 p-1 truncate dark:bg-gray-500 dark:text-gray-200">
+                  {{ note.content }}
+                </div>
+                <div class="text-xs text-gray-600 dark:text-white">
+                  <RelativeDate :date="note.time" />
+                </div>
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div v-else-if="noteType === 'system_message'">
+          <div class="border-b flex justify-between p-1">
+            <div>系统通知</div>
+            <div class="text-sm" :class="{ 'text-gray-500': !listNoteCountUnread }">全部标为已读</div>
+          </div>
+          <div class="divide-y-2">
+            <div v-for="note in listNote" :key="note.id.id">
+              <div
+                v-if="note.__typename === 'SystemNotificationObject'"
+                class="m-1 p-2 shadow rounded-md space-y-2"
+                :class="{ 'bg-gray-100 dark:bg-gray-500': !note.read }"
+              >
+                <div>{{ note.title }}</div>
+                <div class="text-sm">
+                  {{ note.content }}
+                </div>
+                <div class="text-xs text-gray-600 dark:text-white">
+                  <RelativeDate :date="note.time" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <PvPagination
+          v-if="!loading"
+          :page-count="Number(pageCount)"
+          :page="page"
+          @previous="jumpToPreviousPage"
+          @next="jumpToNextPage"
+          @change="jumpToSelectedPage"
+        ></PvPagination>
+      </div>
     </div>
   </LayoutDefault>
 </template>
