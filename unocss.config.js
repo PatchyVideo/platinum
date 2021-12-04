0ef010968789e3295d99804d6ddf50ddddf514fd
QiroNT <i@ntz.im>
2021-12-05 00:45:39 +0800

    fix: tag colors

--- unocss.config.js
+++ unocss.config.js
@@ -11,6 +11,22 @@ export default defineConfig({
     presetIcons(),
   ],
   theme: {
+    colors: {
+      // copyright: '#a0a',
+      // language: '#585455',
+      // character: '#0a0',
+      // author: '#a00',
+      // general: '#0073ff',
+      // meta: '#f80',
+      // soundtrack: '#ff7792',
+      copyright: '#6B006B',
+      language: '#666162',
+      character: '#006B00',
+      author: '#6B0000',
+      general: '#003980',
+      meta: '#804400',
+      soundtrack: '#803C49',
+    },
     breakpoints: {
       sm: '576px',
       md: '720px',
