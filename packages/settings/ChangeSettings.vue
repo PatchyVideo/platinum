d08dd268853f60e70eb0e538529f0e6c5dcf1492
QiroNT <i@ntz.im>
2021-12-10 20:41:51 +0800

    feat(settings): collapse menu on mobile

--- packages/settings/ChangeSettings.vue
+++ packages/settings/ChangeSettings.vue
@@ -1,22 +1,32 @@
 <template>
   <LayoutDefault>
-    <div class="flex flex-col sm:flex-row gap-8 max-w-screen-xl mx-4 sm:mx-auto mt-4">
+    <div class="flex flex-col sm:flex-row gap-x-8 gap-y-4 max-w-screen-xl mx-4 sm:mx-auto mt-4">
       <!-- Left Sidebar -->
       <div class="flex-shrink-0 w-full sm:w-48">
-        <h1 class="text-2xl mb-3" v-text="t('settings.settings')"></h1>
-        <div class="flex flex-col space-y-1">
+        <div class="flex flex-row flex-nowrap justify-between items-center">
+          <h1 class="text-2xl" v-text="t('settings.settings')"></h1>
+          <!-- Mobile Collapse -->
           <div
-            v-for="catogory in catogories"
-            :key="catogory.name"
-            class="pl-2 py-1 rounded-full"
-            :class="{
-              'cursor-default text-pink-800 dark:text-white bg-pink-100 dark:bg-gray-700': active === catogory.name,
-              'cursor-pointer select-none': active !== catogory.name,
-            }"
-            @click="() => active !== catogory.name && router.push('/settings/' + catogory.name)"
-          >
-            <div class="inline-block mr-2 text-4xl text-center align-middle" :class="catogory.icon"></div>
-            <div class="inline-block text-lg align-middle" v-text="catogory.text"></div>
+            class="sm:hidden text-3xl"
+            :class="{ 'i-uil-arrow-from-top': collapsed, 'i-uil-arrow-to-bottom transform rotate-180': !collapsed }"
+            @click="onCollapse"
+          ></div>
+        </div>
+        <div class="overflow-hidden" :class="{ 'lt-sm:h-0': collapsed }">
+          <div class="flex mt-3 flex-col gap-y-1">
+            <div
+              v-for="catogory in catogories"
+              :key="catogory.name"
+              class="pl-2 py-1 rounded-full"
+              :class="{
+                'cursor-default text-pink-800 dark:text-white bg-pink-100 dark:bg-gray-700': active === catogory.name,
+                'cursor-pointer select-none': active !== catogory.name,
+              }"
+              @click="() => active !== catogory.name && router.push('/settings/' + catogory.name)"
+            >
+              <div class="inline-block mr-2 text-4xl text-center align-middle" :class="catogory.icon"></div>
+              <div class="inline-block text-lg align-middle" v-text="catogory.text"></div>
+            </div>
           </div>
         </div>
       </div>
@@ -29,12 +39,13 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, defineAsyncComponent, watchEffect } from 'vue'
+import { computed, defineAsyncComponent, ref, watchEffect } from 'vue'
 import type { Component } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { isLogin, IsLogin } from '@/user'
 import { useI18n } from 'vue-i18n'
 import { setSiteTitle } from '@/common/lib/setSiteTitle'
+import { useThrottleFn } from '@vueuse/core'
 
 const route = useRoute()
 const router = useRouter()
@@ -85,4 +96,13 @@ watchEffect(() => {
     setSiteTitle(`${activeCatogory.value.text} - ${t('settings.settings')} - PatchyVideo`)
   }
 })
+
+const collapsed = ref(true)
+const onCollapse = useThrottleFn(
+  () => {
+    collapsed.value = !collapsed.value
+  },
+  100,
+  false
+)
 </script>
