251a3f9cb6f5f388b094f20ac0492dd3c36066d0
QiroNT <i@ntz.im>
2021-12-13 23:43:36 +0800

    refactor: requireLogin

--- packages/main/main.ts
+++ packages/main/main.ts
@@ -1,5 +1,7 @@
 import { createApp, defineComponent, h, nextTick } from 'vue'
 
+const appPromises: Promise<unknown>[] = []
+
 /* CSS */
 import '@/css'
 
@@ -33,155 +35,6 @@ const app = createApp(
     },
   })
 )
-const appPromises: Promise<unknown>[] = []
-
-/* Vue Router */
-import { createRouter, createWebHistory } from 'vue-router'
-const router = createRouter({
-  history: createWebHistory(),
-  strict: true,
-  routes: [
-    {
-      path: '/',
-      alias: ['/index', '/index.html'],
-      component: () => import('@/home/Home.vue'),
-    },
-    {
-      path: '/search-page',
-      component: () => import('@/search/SearchPage.vue'),
-    },
-    {
-      path: '/search-result',
-      component: () => import('@/search/SearchResult.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/video-list',
-      component: () => import('@/video/VideoList.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/video/:vid',
-      component: () => import('@/video/Video.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/embed/:vid',
-      component: () => import('@/video/Embed.vue'),
-    },
-    {
-      path: '/edit-video/:vid',
-      component: () => import('@/video/EditVideo.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/tag-editor/:vid',
-      component: () => import('@/video/EditVideoTag.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/playlist-list',
-      component: () => import('@/playlist/PlaylistList.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/playlist/:pid',
-      component: () => import('@/playlist/Playlist.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/tag/author/:tid',
-      component: () => import('@/tag/Author.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/leaderboard',
-      component: () => import('@/leaderboard/Leaderboard.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/user/me',
-      component: () => import('@/user/User.vue'),
-    },
-    {
-      path: '/user/notification',
-      component: () => import('@/user-notification/Notification.vue'),
-    },
-    {
-      path: '/user/login',
-      component: () => import('@/user/Login.vue'),
-    },
-    {
-      path: '/user/signup',
-      component: () => import('@/user/Signup.vue'),
-    },
-    {
-      path: '/user/forget-password',
-      component: () => import('@/user/ForgetPassword.vue'),
-    },
-    {
-      path: '/user/reset-password',
-      component: () => import('@/user/ResetPassword.vue'),
-    },
-    {
-      path: '/user/redirect',
-      component: () => import('@/user/Redirect.vue'),
-    },
-    {
-      path: '/user/:uid',
-      component: () => import('@/user/Profile.vue'),
-      meta: { holdLoading: true },
-    },
-    {
-      path: '/settings',
-      redirect: '/settings/general',
-    },
-    {
-      path: '/settings/:catogory',
-      component: () => import('@/settings/ChangeSettings.vue'),
-    },
-    {
-      path: '/debug/error-pages/404',
-      component: () => import('@/error-pages/components/404.vue'),
-    },
-    {
-      path: '/:url+',
-      component: () => import('@/error-pages/components/404.vue'),
-    },
-  ],
-})
-let pendingNProgress: number | undefined
-router.beforeEach(async (to, from, next) => {
-  if (pendingNProgress === undefined)
-    pendingNProgress = setTimeout(() => {
-      if (!NProgress.isStarted()) NProgress.start()
-    }, 100)
-  await appPromisesFinish
-  if (to.path === '/user/notification' && isLogin.value != IsLogin.yes) next({ path: '/' })
-  else next()
-})
-router.afterEach((guard) => {
-  incProcess()
-  checkIfBackendDown()
-  appPromisesFinish.then(() => {
-    if (pendingNProgress) {
-      clearTimeout(pendingNProgress)
-      pendingNProgress = undefined
-    }
-    if (!guard.meta.holdLoading) {
-      if (NProgress.isStarted()) NProgress.done()
-    }
-  })
-})
-app.use(router)
-
-/* Vue I18n */
-import i18n from '@/locales'
-app.use(i18n)
-
-/* Vue Motion */
-import { MotionPlugin } from '@vueuse/motion'
-app.use(MotionPlugin)
 
 /* Check if backend is alive */
 import BackendDown from './components/BackendDown.vue'
@@ -219,7 +72,185 @@ appPromises.push(checkIfBackendDown())
 
 /* Login authentication & user data filling */
 import { checkLoginStatus, isLogin, IsLogin } from '@/user'
-appPromises.push(checkLoginStatus(true))
+const checkLoginStatusPromise = checkLoginStatus(true)
+appPromises.push(checkLoginStatusPromise)
+
+/* Vue Router */
+import { createRouter, createWebHistory } from 'vue-router'
+declare module 'vue-router' {
+  interface RouteMeta {
+    holdLoading?: boolean
+    requireLogin?: boolean
+  }
+}
+const router = createRouter({
+  history: createWebHistory(),
+  strict: true,
+  routes: [
+    // Home
+    {
+      path: '/',
+      alias: ['/index', '/index.html'],
+      component: () => import('@/home/Home.vue'),
+    },
+
+    // Search
+    {
+      path: '/search-page',
+      component: () => import('@/search/SearchPage.vue'),
+    },
+    {
+      path: '/search-result',
+      component: () => import('@/search/SearchResult.vue'),
+      meta: { holdLoading: true },
+    },
+
+    // Video
+    {
+      path: '/video-list',
+      component: () => import('@/video/VideoList.vue'),
+      meta: { holdLoading: true },
+    },
+    {
+      path: '/video/:vid',
+      component: () => import('@/video/Video.vue'),
+      meta: { holdLoading: true },
+    },
+    {
+      path: '/embed/:vid',
+      component: () => import('@/video/Embed.vue'),
+    },
+    {
+      path: '/edit-video/:vid',
+      component: () => import('@/video/EditVideo.vue'),
+      meta: { holdLoading: true },
+    },
+    {
+      path: '/tag-editor/:vid',
+      component: () => import('@/video/EditVideoTag.vue'),
+      meta: { holdLoading: true },
+    },
+
+    // Playlist
+    {
+      path: '/playlist-list',
+      component: () => import('@/playlist/PlaylistList.vue'),
+      meta: { holdLoading: true },
+    },
+    {
+      path: '/playlist/:pid',
+      component: () => import('@/playlist/Playlist.vue'),
+      meta: { holdLoading: true },
+    },
+
+    // Tag
+    {
+      path: '/tag/author/:tid',
+      component: () => import('@/tag/Author.vue'),
+      meta: { holdLoading: true },
+    },
+
+    // LeaderBoard
+    {
+      path: '/leaderboard',
+      component: () => import('@/leaderboard/Leaderboard.vue'),
+      meta: { holdLoading: true },
+    },
+
+    // User
+    {
+      path: '/user/me',
+      component: () => import('@/user/User.vue'),
+    },
+    {
+      path: '/user/notification',
+      component: () => import('@/user-notification/Notification.vue'),
+      meta: { requireLogin: true },
+    },
+    {
+      path: '/user/login',
+      component: () => import('@/user/Login.vue'),
+    },
+    {
+      path: '/user/signup',
+      component: () => import('@/user/Signup.vue'),
+    },
+    {
+      path: '/user/forget-password',
+      component: () => import('@/user/ForgetPassword.vue'),
+    },
+    {
+      path: '/user/reset-password',
+      component: () => import('@/user/ResetPassword.vue'),
+    },
+    {
+      path: '/user/redirect',
+      component: () => import('@/user/Redirect.vue'),
+    },
+    {
+      path: '/user/:uid',
+      component: () => import('@/user/Profile.vue'),
+      meta: { holdLoading: true },
+    },
+
+    // Settings
+    {
+      path: '/settings',
+      redirect: '/settings/general',
+    },
+    {
+      path: '/settings/:catogory',
+      component: () => import('@/settings/ChangeSettings.vue'),
+    },
+
+    // Error
+    {
+      path: '/debug/error-pages/404',
+      component: () => import('@/error-pages/components/404.vue'),
+    },
+    {
+      path: '/:url+',
+      component: () => import('@/error-pages/components/404.vue'),
+    },
+  ],
+})
+let pendingNProgress: number | undefined
+router.beforeEach(async (to, from, next) => {
+  // start progress bar if page took a bit of time to load
+  if (pendingNProgress === undefined)
+    pendingNProgress = setTimeout(() => {
+      if (!NProgress.isStarted()) NProgress.start()
+      pendingNProgress = undefined
+    }, 150)
+
+  await checkLoginStatusPromise
+
+  if (to.meta.requireLogin && isLogin.value !== IsLogin.yes) next({ path: '/' })
+
+  next()
+})
+router.afterEach((guard) => {
+  incProcess()
+  checkIfBackendDown()
+  appPromisesFinish.then(() => {
+    if (pendingNProgress) {
+      clearTimeout(pendingNProgress)
+      pendingNProgress = undefined
+    }
+    if (!guard.meta.holdLoading) {
+      if (NProgress.isStarted()) NProgress.done()
+    }
+  })
+})
+app.use(router)
+
+/* Vue I18n */
+import i18n from '@/locales'
+app.use(i18n)
+
+/* Vue Motion */
+import { MotionPlugin } from '@vueuse/motion'
+app.use(MotionPlugin)
 
 const appPromisesFinish = Promise.allSettled(appPromises.map((v) => v.then(incProcess))).then(() => {
   app.mount('#app')
