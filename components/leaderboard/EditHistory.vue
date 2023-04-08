897dba591b1fa65a3afe2d4d2c2e1611d9e4da33
Lurantis <1510033076@qq.com>
2023-04-08 23:22:52 +0800

    chore(leaderboard): :construction: EditHistory page WIP

---
+++ components/leaderboard/EditHistory.vue
@@ -0,0 +1,26 @@
+<!-- Playlist-list root page -->
+<script lang="ts" setup>
+const props = defineProps<{
+  showAutoPost: boolean
+  page: number
+  limit: number
+}>()
+
+const route = useRoute()
+
+const updatePage = (page: number) => {
+  window.scrollTo(0, 0)
+  navigateTo({ query: { ...route.query, page } })
+}
+</script>
+
+<template>
+  <div>
+    <PPagination
+      class="mt-4"
+      :page="page"
+      :total="50"
+      @update:page="updatePage"
+    />
+  </div>
+</template>
