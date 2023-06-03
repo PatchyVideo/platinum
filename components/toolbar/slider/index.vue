0c2bcb3b401df262d345373a351bcf895b2c5c32
Lurantis <1510033076@qq.com>
2023-06-03 19:44:53 +0800

    style(nav): :art:

--- components/toolbar/slider/index.vue
+++ components/toolbar/slider/index.vue
@@ -16,14 +16,14 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div
-    class="fixed flex flex-col gap-4 max-w-100vw h-100vh z-50 pl-4 pr-10 py-1 bg-white rounded-r-lg transition duration-500 transition-all overflow-auto"
-    :class="show ? 'left-0' : '-left-100'"
-  >
-    <ToolbarLogo v-model:show-slider="show" class="flex-grow-0" />
+  <div>
+    <div
+      class="fixed flex flex-col gap-4 max-w-100vw h-100vh z-50 pl-4 pr-10 py-1 bg-white rounded-r-lg transition duration-500 transition-all overflow-auto"
+      :class="show ? 'left-0' : '-left-100'"
+    >
+      <ToolbarLogo v-model:show-slider="show" class="flex-grow-0" />
 
-    <ToolbarSliderGeneralLinks />
-
-    <PMask v-model:show="show" />
+      <ToolbarSliderGeneralLinks />
+    </div>    <PMask v-model:show="show" />
   </div>
 </template>
